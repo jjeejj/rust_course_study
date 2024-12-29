bbb4637e5d45b807a91ee345d4f9ffafb420c83d
jjeejj <wenjun.jiang@qq.com>
2024-12-29 09:39:09 +0800

    变量解构

--- src/variable.rs
+++ src/variable.rs
@@ -6,6 +6,15 @@ pub fn test_variable() {
     println!("x is {}", x);
     let _y = 5;
 
+    // 解构表达式
     let (a,  b): (bool, bool) = (true, false);
-    println!("a is {:?}, b is {:?}", a, b)
+    println!("a is {:?}, b is {:?}", a, b);
+
+    struct Struct {
+        e: i32
+    }
+    let (a, b, c,d,e);
+    (a,b) = (1,2);
+    (c, .., d, _) = (1,2,3,4,5);
+    Struct { e, .. } = Struct { e: 1 };
 }
