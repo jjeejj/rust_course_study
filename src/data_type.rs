4932a02e32bc3ec1342b269e427ce4dbc9315064
jjeejj <wenjun.jiang@qq.com>
2025-01-01 22:59:25 +0800

    feat: is nan

--- src/data_type.rs
+++ src/data_type.rs
@@ -3,5 +3,12 @@ pub fn test_type() {
     let a: u8 = 255;
     let b = a.wrapping_add(20);
     println!("{}", b);
-    assert_eq!(0.1 + 0.2, 0.3);
+    // assert_eq!(0.1 + 0.2, 0.3);
+
+    let c: f32 = (-42.0_f32).sqrt();
+    if c.is_nan() {
+        println!("c is NaN");
+        return;
+    }
+    assert_eq!(c,c);
 }
